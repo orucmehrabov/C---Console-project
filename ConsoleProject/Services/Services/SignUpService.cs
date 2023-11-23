7899ad4ffa8e7a33e2f2d317d893ceed8afd9f09
Oruj Mehrabov <102570437+orucmehrabov@users.noreply.github.com>
2023-11-23 23:58:38 +0400

    Add Layers

---
+++ ConsoleProject/Services/Services/SignUpService.cs
@@ -0,0 +1,14 @@
+﻿using Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class SignUpService : ISignUpService
+    {
+
+    }
+}
